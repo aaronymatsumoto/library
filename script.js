f659d1ba15f1907c2dbc7a6de92c212a8023d55d
Aaron Matsumoto <aaronymatsumoto@gmail.com>
2021-06-17 20:01:46 -0700

    Complete form submittal function to put new books into myLibrary array

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 function openForm() {
   document.getElementById("popupForm").style.display = "block";
   
-  //apply blur when open
+  //apply blur background when open
 }
 function closeForm() {
   document.getElementById("popupForm").style.display = "none";
 
-  //remove blur when open
+  //remove blur background when open
 
 }
 
-/*
-let myLibrary = [
-
-];*/
+let myLibrary = [];
 
 function Book(title, author, pages, read){
   this.title = title;
@@ -22,23 +19,33 @@ function Book(title, author, pages, read){
   this.read = read;
   this.info = function () {
     return `${this.title} by ${this.author}, ${pages} pages, ${read} yet`
-  }
+  };
 }
+
+function addBookToLibrary() {
+  const myForm = document.getElementById("form");
+  myForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+  
+  const author = document.getElementById("author");
+  const title = document.getElementById("title");
+  const pages = document.getElementById("pages");
+  const read = document.getElementById("read");
+  const newBook = new Book(author, title, pages, read);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+  
+}
+
 
 /*
-function addBookToLibrary() {
-  //take user input and store new book objects into an array
-  myLibrary.push(______);
-}
-
-
 function displayBook() {
 
 }
 */
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read');
-console.log(theHobbit.info());
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'Not Read');
+//console.log(theHobbit.info());
 
-
-
+myLibrary.push(theHobbit);
